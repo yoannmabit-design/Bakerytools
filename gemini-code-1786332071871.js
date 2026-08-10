c1ab74b0434fe9b3f5bd44ace5c8c4d4ff130968
yoannmabit-design <yoannmabit@gmail.com>
2026-08-10 11:30:53 +0800

    Add files via upload

---
+++ gemini-code-1786332071871.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'yoanns-bakery-v2';
+const FILES_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './logo.jpg',
+  './ingredients-master.html',
+  './recettes.html',
+  './prix-de-revient.html',
+  './commandes-demain.html'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
